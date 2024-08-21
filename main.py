8e108bd0580c115ac95b7932a4a2102df687d8b1
eugene-tulu <gntulu@gmail.com>
2024-08-21 23:59:14 +0300

    fix bug

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def check_winnings(columns, lines, bet, values):
     winnings = 0
     winning_lines = []
     for line in range(lines):
-        symbol = columns[0][lines]
+        symbol = columns[0][line]
         for column in columns:
             symbol_to_check = column[line]
             if symbol != symbol_to_check:
